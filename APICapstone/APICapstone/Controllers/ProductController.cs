8e62c3d0a425ab8ec9e93a8f115f4b73fe12658e
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-11-16 18:54:24 -0500

    Add files via upload

---
+++ APICapstone/APICapstone/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICapstone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        namespace ProductAPICapstone.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ProductsController : ControllerBase
+        {
+            private readonly ProductAPI;
+
+            public ProductsController(ProductAPIContext context)
+            {
+                _context = context;
+            }
+        }
+    }
+}
